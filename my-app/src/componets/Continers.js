fd0542f1d81ae7b7442d078348e7cddb40232871
sagar41991 <patelsagar41991@gmail.com>
2022-06-03 22:06:22 +0530

    3--6-22

---
+++ my-app/src/componets/Continers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import App from '../App'
+
+ class Continers extends Component {
+
+    
+  render() {
+    return (
+      <div>continers</div>
+    )
+  }
+}
+export default App
